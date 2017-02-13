eac1cb63504a89d5e31d544413baa1994c69ed25
Francisco Mendoza Sánchez <paco280295@yahoo.com>
2017-02-13 17:38:06 +0100

    Primer Commit
    Enunciado y condiciones

---
+++ chicago.java
@@ -0,0 +1,29 @@
+/*
+Juego Chicago:
+*
+* VÉASE LA DESCRIPCIÓN DEL JUEGO EN EL FOLIO A COLOR "BOLETIN DE NAVIDAD"
+* TENER EN CUENTA:
+1º Seguir la metodología que se sigue en clase (véase en tema 5 la metodología que hay que seguir)
+2º Manejar excepciones si surgiesen en un programa a parte (fijarse en proyecto de 11ivan)
+3º Crear una clase de utilidades separada con métodos que vayan a ser usados tanto en este programa como en otro. ESTE DEBE INCLUiR RESGUARDOS, CONDUCTORES Y, POR SUPUESTO, LA FUNCIONALIDAD EN Si
+4º Seguir las directrices de la POO, es decir, primero hacer los objetos que convengan y luego implementarlos en el PP
+5º Seguir las normas de cohesión y acoplamiento aprendidas
+6º En el PP solo deben aparecer: llamadas, alguna validación y lecturas. PP LIMPITO!
+7º El proceso completo podrá ejecutarse más de una vez
+8º Se deben de validar todas las entradas posibles
+* 
+* ANALISIS:
+* 
+* Proceso: Se trata de un juego de dados, donde el que saca la puntuación más alta gana la apuesta. Habrá dos dados para cada jugador, 
+* 
+* 
+ */
+
+
+public class  chicago {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
